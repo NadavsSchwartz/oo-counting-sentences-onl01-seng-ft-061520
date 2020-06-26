fb4cb051985b2e36cb8ddb904eb6948b2babaf76
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 04:04:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+    splitter = self.split(" ")
+    splitter.each do |index|
+      if index.end_with?(".")
+      counter += 1
+    end
+      if index.end_with?("!")
+      counter += 1
+    end
+      if index.end_with?("?")
+      counter += 1
+      end
+    end
+    counter
   end
 end
